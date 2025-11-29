82030f4a48c413a528bcd57f248f3d25f8eee1f3
TheTuso <piotrekpasztor@gmail.com>
2025-11-29 14:27:38 +0000

    Initial Commit

---
+++ AxPlayground/AxPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxPlaygroundApp.swift
+//  AxPlayground
+//
+//  Created by Piotr Pasztor on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AxPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
